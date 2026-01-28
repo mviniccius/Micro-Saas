f8545f69c30b2eacc7682de7793a5b52396bac92
mviniccius <marcus.viniccius@hotmail.com>
2026-01-27 22:17:48 -0300

    criacao de nova tabela do banco

--- Back/db/init.sql
+++ Back/db/init.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(120) NOT NULL UNIQUE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+
+ CREATE TABLE public.produtos
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    nome_produto character varying(255) NOT NULL,
+    preco numeric(10, 2),
+    PRIMARY KEY (id)
+);
